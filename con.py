b1c46731b70b8847696bbaede97442dede229f31
Kushagra S <kushagra.rigel@gmail.com>
2020-10-21 12:52:30 +0530

    Update con.py

--- con.py
+++ con.py
@@ -1,12 +1,15 @@
-n = int(input("Enter a number : "))
 while True:
-	if n % 2 == 0 and n != 1:
-		n = n/2
-		print(int(n),"- ",end="")
-	elif n % 2 != 0 and n != 1:
-		n = (3*n) + 1
-		print(int(n),"- ",end ="")
-	elif n == 1:
-		print()
+	i = input("Enter a number ('e' to exit) : ")
+	n = int(i)
+	while True:
+		if n % 2 == 0 and n != 1:
+			n = n/2
+			print(int(n),"- ",end="")
+		elif n % 2 != 0 and n != 1:
+			n = (3*n) + 1
+			print(int(n),"- ",end ="")
+		elif n == 1:
+			print()
+			break
+	if i == "e":
 		break
-
